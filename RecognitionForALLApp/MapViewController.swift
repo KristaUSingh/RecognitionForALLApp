da9bb9c22e8b2a9755267bbeeb6d0a81f3d6f470
Krista Singh <krista.u.singh101@gmail.com>
2022-07-20 15:20:08 -0400

    Added map with pins on Guyana and Sudan and finished Guyana storyboard!

---
+++ RecognitionForALLApp/MapViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MapViewController.swift
+//  RecognitionForALLApp
+//
+//  Created by Krista Singh on 7/20/22.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Guyana coordinates: 4.8604° N, 58.9302° W
+        
+        let guyana = MKPointAnnotation()
+        guyana.coordinate = CLLocationCoordinate2D(latitude: 4.8604, longitude: -58.9302)
+        guyana.title = "Guyana"
+        guyana.subtitle = "The Land of Many Waters"
+        map.addAnnotation(guyana)
+        
+        
+        //sudan coordinates: 12.8628° N, 30.2176° E
+        
+        let sudan = MKPointAnnotation()
+        sudan.coordinate = CLLocationCoordinate2D(latitude: 12.8628, longitude: 30.2176)
+        sudan.title = "Sudan"
+        sudan.subtitle = "Victory is Ours"
+        map.addAnnotation(sudan)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
